d311f236e3332550874baabcdcf58c906b6a1b34
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-12-17 15:52:21 +0100

    asd

--- dist/app.js
+++ dist/app.js
@@ -93,6 +93,17 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+/*
+
+REPO: html-sass-first
+GOAL: in NUOVO progetto NPM, definire semplice layout con header, footer, navbar e content, posizionati correttamente, ogniuno con un colore di background differente e con una scritta in centro che ne riporti il tipo di elemento (header, footer, ecc). Si raccomanda di sfruttare il SASS il piu' possibile, con particolare attenzione alla suddivisione in files dei fogli di stile.
+ATTENZIONE!!
+SI PREGA DI AGGIUNGERE UN FILE DI TESTO NELLA RADICE DEL PROGETTO (dove si usa npm per capirsi) CON ESATTAMENTE QUESTO NOME
+.gitignore
+ED ESATTAMENTE QUESTO CONTENUTO TESTUALE
+/node_modules
+
+*/
 console.log('hello V');
 
 /***/ }),
